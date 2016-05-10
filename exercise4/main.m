793bac65a6a8b61735eaf13931349ddba4f4193d
Yzoni <yzonix@gmail.com>
2016-05-10 13:31:38 +0200

    hoi

--- exercise4/main.m
+++ exercise4/main.m
@@ -14,14 +14,14 @@ im_billboard = im2double(rgb2gray(imread('billboard.png')));
 rows_shapes = 500;
 cols_shapes = 500;
 h_shapes = hough(im_shapes, [0.2, 0.8], rows_shapes, cols_shapes);
-%h_box = hough(im_box, [0.1, 0.9], 100, 100);
-h_szeliski = hough(im_szeliski, [0.2, 0.8], 100, 100);
-%h_billboard = hough(im_billboard, [0.1, 0.9], 100, 100);
+%h_box = hough(im_box, [0.1, 0.9], cols_shapes, cols_shapes);
+h_szeliski = hough(im_szeliski, [0.2, 0.8], rows_shapes, cols_shapes);
+%h_billboard = hough(im_billboard, [0.1, 0.9], cols_shapes, cols_shapes);
 
 imtool(h_shapes, [0,80])
-%imtool(h_box, [])
-%imtool(h_szeliski, [])
-%imtool(h_billboard, [])
+%imtool(h_box, [0,80])
+%imtool(h_szeliski, [0,80])
+%imtool(h_billboard, [0,80])
 
 %%  Finding the Lines as Local Maxima
 [lines, coordinates] = houghlines(im_shapes, h_shapes, 130);
