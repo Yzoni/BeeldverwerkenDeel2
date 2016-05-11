0406d83432dbed34d5c70067c29a9efa0d1e5c3a
Yzoni <yzonix@gmail.com>
2016-05-11 12:04:16 +0200

    hoi

--- exercise4/main.m
+++ exercise4/main.m
@@ -24,30 +24,82 @@ h_szeliski = hough(im_szeliski, [0.2, 0.8], rows_shapes, cols_shapes);
 %imtool(h_billboard, [0,80])
 
 %%  Finding the Lines as Local Maxima
-[lines, coordinates] = houghlines(im_shapes, h_shapes, 130);
 
+%% Only houghline
+[lines, coordinates] = houghlines(im_shapes, h_shapes, 125);
 imshow(im_shapes)
 hold on
-% for n=1:length(coordinates)
-n = 2;   
-line([coordinates(n,1),coordinates(n,2)],[coordinates(n,3),coordinates(n,4)]);
-% end
+for n=1:length(coordinates)
+    line([coordinates(n,1),coordinates(n,2)],[coordinates(n,3),coordinates(n,4)]);
+end
 hold off;
+
+%% points of line
 [x, y] = find(edges_shapes);
 X = vertcat(x', y', ones(1, length(x)));
 
 figure;
 imshow(im_shapes)
-pointofline = points_of_line(X, lines(n,:), 0.01);
-pointofline = pointofline(1:2,:);
-hold on;
-plot(pointofline(2,:),pointofline(1,:),'go');
 
+hold on;
+for n=1:length(lines)
+    pointofline = points_of_line(X, lines(n,:), 0.01);
+    pointofline = pointofline(1:2,:);
+    plot(pointofline(2,:), pointofline(1,:),'go');
+end
 hold off;
 
-[lineshizzle, coordinates] = line_through_points(pointofline, im_shapes);
+%% Total square
 figure;
 imshow(im_shapes)
 hold on
+[lineshizzle, coordinates] = line_through_points(pointofline, im_shapes);
 line([coordinates(1,3),coordinates(1,4)],[coordinates(1,1),coordinates(1,2)]);
 hold off
+
+
+%% 6 Using the Lines
+
+% Implementation of getting the intersection of two lines
+% LINE 1
+n = 2;
+[lines1, coordinates] = houghlines(im_shapes, h_shapes, 130);
+% points of line
+[x, y] = find(edges_shapes);
+X = vertcat(x', y', ones(1, length(x)));
+pointofline = points_of_line(X, lines1(n,:), 0.01);
+pointofline = pointofline(1:2,:);
+% Total square
+[line1, coordinates1] = line_through_points(pointofline, im_shapes);
+
+% LINE 2
+n = 5;
+[lines2, coordinates] = houghlines(im_shapes, h_shapes, 130);
+% points of line
+[x, y] = find(edges_shapes);
+X = vertcat(x', y', ones(1, length(x)));
+pointofline = points_of_line(X, lines2(n,:), 0.01);
+pointofline = pointofline(1:2,:);
+% Total square
+[line2, coordinates2] = line_through_points(pointofline, im_shapes);
+
+% Intersection of lines
+intersection = cross([line1(2, 1); line1(1, 1); line1(3, 1)], [line2(2, 1); line2(1, 1); line2(3, 1)])
+
+% Normalize
+intersection = intersection ./ sqrt(intersection(1)^2 + intersection(2)^2);
+intersection = intersection ./ intersection(3);
+
+figure;
+imshow(im_shapes)
+hold on;
+% intersecting lines
+line([coordinates1(1,3),coordinates1(1,4)],[coordinates1(1,1),coordinates1(1,2)]);
+line([coordinates2(1,3),coordinates2(1,4)],[coordinates2(1,1),coordinates2(1,2)]);
+
+%point
+plot(intersection(2,1), intersection(1,:),'r*');
+hold off;
+
+
+
